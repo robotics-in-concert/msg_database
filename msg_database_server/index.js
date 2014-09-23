b0de09c7bd5be134f329cd256089956566ef9cf8
Eunsub Kim <eunsub@gmail.com>
2014-09-23 15:28:05 +0900

    add dependencies

--- msg_database_server/index.js
+++ msg_database_server/index.js
@@ -1,1 +1,50 @@
-console.log("msg_database_server");
+var _ = require('lodash'),
+  MongoClient = require('mongodb').MongoClient,
+  express = require('express'),
+  exec = require('child_process').exec,
+  CronJob = require('cron').CronJob;
+
+
+MongoClient.connect(process.env.MONGO_URL, function(e, db){
+  if(e) throw e;
+  console.log('mongo connected');
+
+  var app = express();
+
+  server = app.listen(process.env.PORT, function(){
+    console.log('Listening on port %d (%s)', server.address().port, process.env.NODE_ENV);
+  });
+
+
+  app.get('/api/ping', function(req, res){
+    res.send("pong");
+
+  });
+
+
+  var job = new CronJob({
+    cronTime: '0 0 * * * *', // every hour
+    onTick: function(){
+      console.log('sync!');
+      // 1. rosdistro
+      // 2. rosmsg list
+      // 3. python script
+      // 4. update db (+rev info)
+
+
+      // example, run external command
+      //
+      // exec("ls -al", function(e, stdout, stderr){
+        // console.log(stdout);
+      // });
+
+
+    },
+    start: false
+  });
+
+
+
+});
+
+
