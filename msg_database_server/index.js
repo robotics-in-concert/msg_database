57cf77cc60ef77778376fe6df25c54c0a7d81134
Eunsub Kim <eunsub@gmail.com>
2014-09-30 15:10:22 +0900

    recur

--- msg_database_server/index.js
+++ msg_database_server/index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash'),
   MongoClient = require('mongodb').MongoClient,
+  async = require('async'),
   express = require('express'),
   CronJob = require('cron').CronJob;
 
@@ -27,12 +28,45 @@ MongoClient.connect(process.env.MONGO_URL, function(e, db){
   });
 
   app.get('/api/message_details', function(req, res){
-    var type = req.param.type;
-    
-    // TODO : fetch related types recursively
-    coll.findOne({type: type}, function(e, row){
-      res.send(row);
+    var type = req.query.type;
+    if(_.isEmpty(type)) return res.status(400).send('error');
+
+    (function _fetch(type, done){
+      console.log("fetching "+type);
+
+      var results = []
+      coll.findOne({type: type}, function(e, row){
+        if(e) return done(e);
+        var detail = row.detail;
+        results.push(detail);
+        var subTypes = _.select(row.detail.fieldtypes, function(t){
+          return t.indexOf('/') >= 0;
+        });
+
+        if(!_.isEmpty(subTypes)){
+
+          async.reduce(subTypes, results, function(memo, t, redcb){
+            _fetch(t, function(e, res){
+              redcb(null, memo.concat(res));
+            });
+
+          }, function(err, final){
+            done(null, final);
+
+          });
+
+        }else{
+          done(null, results);
+        }
+
+      });
+
+    })(type, function(e, lst){
+      console.log("cnt", lst.length);
+
+      res.json(lst);
     });
+    
 
   });
 
