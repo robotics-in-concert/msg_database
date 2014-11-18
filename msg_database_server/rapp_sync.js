222eb10c58f7497def39d9ebd045e4dda0431753
Eunsub Kim <eunsub@gmail.com>
2014-11-18 13:56:57 +0900

    parse rapp, interface

--- msg_database_server/rapp_sync.js
+++ msg_database_server/rapp_sync.js
@@ -26,30 +26,162 @@ extract_rapp_meta = function(url, callback){
     path: dest
   }));
   tarstrm.on('end', function(){
-    glob(dest + "/**/package.xml", function(e, files){
-      console.log(files);
+    // packages
+    //
+    //
+    async.waterfall([
+      // detect package.xml
+      function(cb){ glob(dest + "/**/package.xml", cb); },
 
-      var data = async.map(files, function(f, cb0){
-        xml2js.parseString(fs.readFileSync(f, 'utf8'), {async: true, mergeAttrs: true}, function(e2, js){
-          cb0(e2, js.package);
+      // parse package.xml
+      function(package_files, cb){ 
+
+        async.map(package_files, function(f, cb2){
+          var package_base = Path.dirname(f);
+          console.log(package_base);
+          xml2js.parseString(fs.readFileSync(f, 'utf8'), {async: true, mergeAttrs: true}, function(e2, js){
+            var package = js.package;
+            cb2(null, {base: package_base, package: package});
+          });
+
+        }, function(e, package_lst){
+          cb(null, package_lst); 
+        });
+      },
+
+
+      // load rapp
+      //
+      function(package_list, cb){
+        async.map(package_list, function(package_info, cb2){
+          // rapp
+
+          var rocon_apps = package_info.package.export[0].rocon_app;
+          var rapps = _.map(rocon_apps, function(rapp){
+            var f = Path.join(package_info.base, rapp);
+            if(fs.existsSync(f)){
+
+              var rapp_meta = fs.readFileSync(f, 'utf8');
+
+
+              var rapp_meta = _.reduce(rapp_meta.split(/\n/), function(memo, line){
+                var kv = line.split(":");
+                var key = kv.shift();
+                var value = kv.join(":");
+                memo[key] = value.trim();
+                return memo;
+              }, {});
+              console.log(rapp_meta);
+
+            }
+
+
+
+
+
+            // var doc = yaml.safeLoad(fs.readFileSync(f, 'utf8'));
+            return rapp_meta;
+          });
+
+          package_info.rocon_apps = rapps;
+          cb2(null, package_info);
+
+
+
+        }, function(e, results){
+          console.log('xxxxxxxxxxxxxx', results);
+
+          cb(null, results);
+          
+        });
+      }, 
+        
+      function(package_list, cb){
+        async.map(package_list, function(package_info, cb2){
+          var rocon_apps = package_info.package.export[0].rocon_app;
+          var interfaces = _.map(rocon_apps, function(rapp){
+            var f = Path.join(package_info.base, rapp).replace(/rapp$/, "interface");
+            if(fs.existsSync(f)){
+              var doc = yaml.safeLoad(fs.readFileSync(f, 'utf8'));
+            }
+            return doc;
+          });
+          package_info.interfaces = interfaces;
+          cb2(null, package_info);
+
+        }, function(e, lst){
+          cb(null, lst);
         });
 
-      }, function(e, lst){
-        console.log("------------------------");
-        console.log(lst);
-
-        console.log("------------------------");
-
-        callback(null, lst);
-        rimraf.sync(dest);
+      }
         
-      });
-      // clean
+        
 
+
+
+    ], function(e, results){
+      console.log('------------------------------');
+      console.log(_.omit(results, 'base'));
+      console.log('------------------------------');
+      rimraf.sync(dest);
 
 
     });
   });
+    // glob(dest + "/**/package.xml", function(e, files){
+      // console.log(files);
+
+      // var data = async.map(files, function(f, cb0){
+        // var package_base = Path.dirname(f);
+        // console.log(package_base);
+
+        // xml2js.parseString(fs.readFileSync(f, 'utf8'), {async: true, mergeAttrs: true}, function(e2, js){
+          // var package_meta = js.package;
+
+
+          // console.log(package_meta.export);
+
+
+
+          // var rapps = package_meta.export[0].rocon_app;
+          // console.log(rapps);
+
+
+          // _.each(rapps, function(rapp_path){
+            // Path.join(package_base, rapp_path)
+
+            // // interfaces
+            // glob(dest + "/**/*interface", function(e, files){
+              // var data = _.map(files, function(f){
+                // var doc = yaml.safeLoad(fs.readFileSync(f, 'utf8'));
+                // return doc;
+              // });
+              // // clean
+              // rimraf.sync(dest);
+              // callback(null, data);
+
+
+
+            // });
+
+          // });
+
+
+
+          // cb0(e2, js.package);
+        // });
+
+      // }, function(e, lst){
+        // callback(null, lst);
+        // rimraf.sync(dest);
+        
+      // });
+      // // clean
+
+
+
+    // });
+  // });
 
 
 
