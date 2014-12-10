cd825475400fe10ac8e0975a54df4f0f6b677933
Eunsub Kim <eunsub@gmail.com>
2014-12-10 11:39:13 +0900

    load client apps

--- msg_database_server/client_apps_sync.js
+++ msg_database_server/client_apps_sync.js
@@ -8,30 +8,16 @@ var Promise = require('bluebird'),
 
 LIST_URL = "https://gist.githubusercontent.com/eskim/78e3822a1fc31ebe2fbe/raw"
 
-
-
-
 var doSync = function(db){
 
-  var _load_yaml = function(url){
-    return request(url).then(R.compose(yaml.safeLoad, R.nth(1)));
-  };
 
   return request(LIST_URL)
-    .then(R.compose(R.split(/\n/), R.nth(1))
+    .then(R.compose(R.split(/\n/), R.nth(1)))
     .map(function(url){
-      return _load_yaml(url)
-        .then(function(client_apps_list_meta){
-          var interactions = client_apps_list_meta.interactions;
-          var x = R.map(utils.resolve_url(url))(interactions);
-          console.log('X', x);
-
-          return x;
-        })
-
+      return utils.load_yaml(url)
+        .then( R.compose( R.map(utils.resolve_url(url)), R.prop('interactions')) )
     })
-    .then(R.flatten)
-    .map(_load_yaml);
+    .then(R.unnest).map(utils.load_yaml).then(R.unnest)
 };
 
 
--- msg_database_server/utils.js
+++ msg_database_server/utils.js
@@ -1,10 +1,17 @@
 var util = require('util'),
     R = require('ramda'),
+    Promise = require('bluebird'),
+    request = Promise.promisify(require('request')),
+    yaml = require('js-yaml'),
     URL = require('url');
 
 
 
 module.exports = {
+  load_yaml: function(url){
+    return request(url).then(R.compose(yaml.safeLoad, R.nth(1)));
+  },
+
   inspect: function(x){
     console.log(util.inspect(x, false, 10, true));
   },
