e0ebf5b1d02f8d51bdeaa2b143220b16b7b0534f
Eunsub Kim <eunsub@gmail.com>
2014-12-10 14:21:21 +0900

    add interactions to mongo

--- msg_database_server/client_apps_sync.js
+++ msg_database_server/client_apps_sync.js
@@ -17,15 +17,26 @@ var doSync = function(db){
       return utils.load_yaml(url)
         .then( R.compose( R.map(utils.resolve_url(url)), R.prop('interactions')) )
     })
-    .then(R.unnest).map(utils.load_yaml).then(R.unnest)
+    .then(R.unnest)
+    .map(utils.load_yaml)
+    .then(R.unnest);
 };
 
 
 
 module.exports = exports = function sync(db){
-  doSync(db).then(function(res){
-    console.log('Done.');
-    utils.inspect(res);
+  var coll = db.collection('interactions');
+  doSync(db).map(function(interaction){
+    utils.inspect(interaction);
+    console.log('updating interaction : ', interaction.name);
+
+
+    coll.update({name: interaction.name}, {$set: interaction}, {w:1, upsert: true}, function(e, x){
+      console.log('updated interaction : ', interaction.name);
+    });
+
+
+
   })
   .catch(function(e){
     console.error(e);
