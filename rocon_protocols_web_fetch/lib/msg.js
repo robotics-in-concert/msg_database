30a71c869e1577562ac3e9c14697c8d21ac2b68e
Eunsub Kim <eunsub@gmail.com>
2015-02-04 19:03:42 +0900

    limit concurrency

--- rocon_protocols_web_fetch/lib/msg.js
+++ rocon_protocols_web_fetch/lib/msg.js
@@ -3,16 +3,20 @@ var Promise = require('bluebird'),
   exec = Promise.promisify(require('child_process').exec);
 
 
-module.exports = exports = function(callback){
+module.exports = exports = function(callback, concurrency){
+  if(!concurrency){
+    concurrency = 4;
+  }
   return exec('rosmsg list')
     .spread(function(out, err){
       return out.trim().split(/\n/);
     })
     .map(function(type){
       return exec("rosrun rocon_protocols_web_scripts convert_msg_spec_to_json.py "+type).spread(function(out){
+        // callback(null, [JSON.parse(out)]);
         return JSON.parse(out);
       });
-    })
+    }, {concurrency: concurrency})
     .done(
       function(data){ callback(null, data); },
       function(e){ callback(e); }
