8e5488d535ba3eb267cfbe6baf5e17a1f4faa28b
Eunsub Kim <eunsub@gmail.com>
2015-04-06 15:15:31 +0900

    select type control

--- rocon_protocols_webserver/public/js/app.js
+++ rocon_protocols_webserver/public/js/app.js
@@ -3,8 +3,15 @@ console.log(angular);
 
 angular.module('protocols-web', [
   'ui.bootstrap',
-  'ui.router'
+  'ui.router',
+  'ui.select',
+  'ngSanitize'
 ])
+  .config(function(uiSelectConfig){
+    // uiSelectConfig.theme = 'select2';
+    // uiSelectConfig.resetSearchInput = true;
+
+  })
   .controller('messagesController', require('./ctrls/messages_controller'))
 
 
